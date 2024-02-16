354cb2869861995b59341a6e94d1f6ec0ffa2c69
stevesoniw <iwz.son@gmail.com>
2024-02-16 21:00:51 +0900

    GNB Menu attached

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # son's config.py
-FINNHUB_KEY = ""
-OPENAI_API_KEY = "sk-"
-FRED_API_KEY = ""
-RAPID_API_KEY = ""
+#FINNHUB_KEY = ""
+#OPENAI_API_KEY = "sk-"
+#FRED_API_KEY = ""
+#RAPID_API_KEY = ""
+
